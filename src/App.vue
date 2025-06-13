5aff032f6257d548ed40225e10af5d6f44002a19
bokorni <lucasbokorni@gmail.com>
2025-06-13 18:51:12 -0300

    FEAT: Criação Login e Register Page

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <script setup>
  import HeaderDesktop from './components/HeaderDesktop.vue';
  import FooterPage from './components/FooterPage.vue';
+ import LoginPage from './components/LoginPage.vue';
+ import RegisterPage from './components/RegisterPage.vue';
+ 
 </script>
 
 <template>
-
+        
         <HeaderDesktop />
+         <LoginPage />
+         <RegisterPage />
         <FooterPage />
-
 </template>
 
 <style scoped>
